c709b2dee6c042031e418ed89164f4dce78af6f9
Ryan Sartor <ryanmsartor@gmail.com>
2025-04-08 14:20:59 -0500

    reduce code and blob duplication by moving bins and scripts into .pakui and having libretro-based systems redirect to one single source of truth universal launch script

---
+++ .pakui/constants.sh
@@ -0,0 +1,11 @@
+#/bin/sh
+
+export PAKUI=/mnt/SDCARD/.pakui
+
+export EVTEST="$PAKUI/evtest"
+export AUTORESUME_SCRIPT="$PAKUI/autoresume.sh"
+export FONT_FILE="$PAKUI/minui.ttf"
+export PICKER="$PAKUI/picker"
+export SHOW="$PAKUI/show_message"
+export LAUNCHER_BIN="$PAKUI/universal_launcher"
+export LAUNCHER_SCRIPT="$PAKUI/uni_launcher.sh"
